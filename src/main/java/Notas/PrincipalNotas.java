bf4288a3fc7b178bb8703dfc6b3d15118ed80211
Jmedinabe <jmedinabe@unal.edu.co>
2024-09-09 16:34:32 -0500

    Initial commit

---
+++ src/main/java/Notas/PrincipalNotas.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Notas;
+
+/**
+ *
+ * @author Juan
+ */
+public class PrincipalNotas {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal= new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+    }
+}
